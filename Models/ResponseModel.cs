134b9dd999679695c1caeb63e326cd707f52b51c
Thalles Batista <thallesaugusto.mb@gmail.com>
2025-08-11 17:51:33 -0300

    feat: Add models

---
+++ Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DapperUserApi.Models
+{
+    public class ResponseModel<T>
+    {
+        public T? Data { get; set; }
+        public string Messages { get; set; } = string.Empty;
+        public bool Status { get; set; } = true;
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace DapperUserApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+        public string TaxNumber { get; set; }
+        public bool Situation { get; set; }
+        public string Password { get; set; }
+    }
+}
