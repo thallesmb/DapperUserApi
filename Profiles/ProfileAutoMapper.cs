b7fd6ccc607607c98099cd370f4fc3650d77a105
Thalles Batista <thallesaugusto.mb@gmail.com>
2025-08-13 22:26:21 -0300

    Feat: Add AutoMapper

---
+++ Profiles/ProfileAutoMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DapperUserApi.DTO;
+using DapperUserApi.Models;
+
+namespace DapperUserApi.Profiles
+{
+    public class ProfileAutoMapper : Profile
+    {
+        public ProfileAutoMapper()
+        {
+            CreateMap<User, UserListDTO>();
+        }
+    }
+}
