ade19e7d3c9a238c364dd6563f7de12a19277929
Thalles Batista <thallesaugusto.mb@gmail.com>
2025-08-13 22:25:56 -0300

    Fix: Alter DTO

--- DTO/UserListDTO.cs
+++ DTO/UserListDTO.cs
@@ -2,12 +2,11 @@
 {
     public class UserListDTO
     {
+        public int Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
         public string Position { get; set; }
         public double Salary { get; set; }
-        public string TaxNumber { get; set; }
         public bool Situation { get; set; } = true;
-        public string Password { get; set; }
     }
 }
