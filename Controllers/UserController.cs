3d26469b0442d9f8cc9f7f1ada9512afe32c03b5
Thalles Batista <thallesaugusto.mb@gmail.com>
2025-08-14 22:51:38 -0300

    feat: Add controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using DapperUserApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperUserApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserInterface _userInterface;
+
+        public UserController(IUserInterface userInterface)
+        {
+            _userInterface = userInterface;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _userInterface.GetUserList();
+
+            if(users.Status == false)
+                return NotFound();
+
+            return Ok(users);
+        }
+    }
+}
