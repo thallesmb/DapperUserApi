fd371a5535ca9fc8dd42e00b7d0c0120c775157f
Thalles Batista <thallesaugusto.mb@gmail.com>
2025-08-17 18:46:12 -0300

    refactor: Rename Id

--- DTO/AlterUserDTO.cs
+++ DTO/AlterUserDTO.cs
@@ -2,7 +2,7 @@
 {
     public class AlterUserDTO
     {
-        public int UserId { get; set; }
+        public int Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
         public string Position { get; set; }
