77707eed9b7b9fbda39e7b8684d07f10a68824e7
Thalles Batista <thallesaugusto.mb@gmail.com>
2025-08-17 18:51:12 -0300

    feat: CreateUser and AlterUser endpoints

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,4 @@
-﻿using DapperUserApi.Models;
+﻿using DapperUserApi.DTO;
 using DapperUserApi.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -33,6 +33,39 @@ namespace DapperUserApi.Controllers
 
             if (user.Status == false)
                 return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserDTO createUserDTO)
+        {
+            var user = await _userInterface.CreateUser(createUserDTO);
+
+            if (user.Status == false)
+                return BadRequest(user);
+
+            return Ok(user);
+        }
+
+        [HttpDelete("{userId}")]
+        public async Task<IActionResult> RemoveUser(int userId)
+        {
+            var user = await _userInterface.RemoveUser(userId);
+
+            if (user.Status == false)
+                return BadRequest(user);
+
+            return Ok(user);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> AlterUser(AlterUserDTO alterUserDTO)
+        {
+            var user = await _userInterface.AlterUser(alterUserDTO);
+
+            if (user.Status == false)
+                return BadRequest(user);
 
             return Ok(user);
         }
